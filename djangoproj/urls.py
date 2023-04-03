1410a80116e21ab59bdbb4cfafda8b96ed3ac86e
Ala404 <alaeddine.kezzouz@univ-constantine2.dz>
2023-04-03 12:48:39 +0000

    urls.py for th project is modified

--- djangoproj/urls.py
+++ djangoproj/urls.py
@@ -6,24 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('profiles.urls')),
-    path('', include('adminApps.urls')),
-    ###### Prof#########
-    path('', include('profs.urls')),
-    ###### Module#######
-    path('', include('modules.urls')),
-    ###### Section#######
-    path('', include('sections.urls')),
-    ###### Schedule#######
-    path('', include('schedules.urls')),
-    path('', include('sessionApps.urls')),
-    path('', include('classrooms.urls')),
-    path('', include('students.urls')),
-    path('', include('absences.urls')),
-    path('', include('justifications.urls')),
-    path('', include('exclusions.urls')),
-    path('', include('profiles.urls')),
-    path('', include('quizzy.urls')),
     # path('', include('authentication.urls')),
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
