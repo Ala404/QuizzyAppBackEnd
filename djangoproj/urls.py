bbaff512caf33b29a5ae29bdcc117b79a7e15b65
Ala404 <alaeddine.kezzouz@univ-constantine2.dz>
2023-04-03 12:51:02 +0000

    urls.py modified

--- djangoproj/urls.py
+++ djangoproj/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('quizzy.urls')),
     # path('', include('authentication.urls')),
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
